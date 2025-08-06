a79a9b9a08ef52592037e49394c1ab7643e14bb8
kishikuun <74188233+kishikuun@users.noreply.github.com>
2025-08-06 07:46:13 +0700

    Add files via upload

---
+++ Adguard.js
@@ -0,0 +1,10 @@
+var response = JSON.parse($response.body);
+response = {
+  "products": [
+    {
+      "premium_status": "ACTIVE",
+      "product_id": "com.adguard.lifetimePurchase"
+    }
+  ]
+};
+$done({ body: JSON.stringify(response) });
